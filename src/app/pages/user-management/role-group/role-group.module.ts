c4a69898758801ca042444d342f432f38c9f6da0
JackyTran <bachtuvu.dev@gmail.com>
2022-04-04 16:28:30 +0700

    update server to deploy angular

--- src/app/pages/user-management/role-group/role-group.module.ts
+++ src/app/pages/user-management/role-group/role-group.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RoleGroupRoutingModule } from './role-group-routing.module';
-import { RoleGroupComponent } from './role-group/role-group.component';
 import { RoleGroupFrmComponent } from './role-group-frm/role-group-frm.component';
+import { RoleGroupComponent } from './role-group.component';
 
 
 @NgModule({

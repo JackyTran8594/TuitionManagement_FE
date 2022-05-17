4f1ddf397cabc8f71c73f4521f262cd2588b7819
unknown <bachtuvu.dev@gmail.com>
2022-05-18 00:12:49 +0700

    update

--- src/app/auth/service/auth.api.ts
+++ src/app/auth/service/auth.api.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpService } from '../../shared/http.service';
-import {AccessToken, User } from './auth';
+import { AccessToken, User } from './auth';
 
 @Injectable()
 export class AuthApi {
 
-  private readonly apiController: string = 'authenticate';
+  // private readonly apiController: string = 'authenticate';
+  private readonly apiController: string = 'auth';
 
   token: AccessToken = {};
 
@@ -16,11 +17,11 @@ export class AuthApi {
   }
 
   authenticate(item: User): Observable<AccessToken> {
-    return this.http.post(this.apiController, item);
+    return this.http.post(this.apiController + "/login", item);
   }
 
 
-  
+
 
 
 

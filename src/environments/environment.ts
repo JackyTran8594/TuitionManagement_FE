0b9011a91c7e333dadcb955216329ec9b4552374
unknown <bachtuvu.dev@gmail.com>
2022-05-19 00:20:22 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://api02-dat.bhq.vn:9082/api',
-  // apiUrl:'http://localhost:8080/api',
+  // apiUrl:'http://api02-dat.bhq.vn:9082/api',
+  apiUrl:'http://localhost:8082/api',
 
 };
